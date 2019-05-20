e8e99539fce34f12b6cdd1a1cf36575bf75ee50f
Edison Arango <edisonarab@gmail.com>
2019-05-20 13:38:38 -0500

    update django version to 2.2.1

--- src/manage.py
+++ src/manage.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-if __name__ == '__main__':
+def main():
     if 'PRODUCTION' in os.environ:
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_name.settings.production")
     else:
@@ -16,3 +16,6 @@ if __name__ == '__main__':
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+    main()
